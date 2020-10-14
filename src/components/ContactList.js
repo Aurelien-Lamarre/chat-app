48a9086bdc42594b73d45bc538e21081b49e0ccd
Aurelien <lamarreaurelien@outlook.fr>
2020-10-14 09:20:39 +0200

    modif contactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Contact';
 
 const users = [
@@ -29,22 +29,49 @@ const users = [
     }
   ];
 
-const ContactList = () => {
+// const ContactList = () => {
+//     return (
+//         users.map(profile => (
+//             <div className="Contact">
+//             <img class='avatar' src={profile.avatar}/>
+//             <div className='status'>
+//             <h4 className='name'>{profile.name}</h4>
+//             <div className={this.state.online ? 'status-online': 'status-offline'}></div>
+//                 <p className='status-text' onClick={event => {
+//                     const offline = !this.state.online;
+//                     this.setState({online:offline})
+//                 }}>{this.state.online ? 'Online': 'Offline'} </p>
+//             </div>
+//         </div>
+//         ))
+
+//     )
+// }
+
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      online: false
+    };
+  }
+  render() {
     return (
-        users.map(profile => (
-            <div className="Contact">
-            <img class='avatar' src={profile.avatar}/>
-            <div className='status'>
-            <h4 className='name'>{profile.name}</h4>
-            <div>
-                <div className='status-online'></div>
-                <p className='status-text'>{profile.online ? 'Online': 'Offline'}</p>
-            </div>
-            </div>
-        </div>
-        ))
-
-    )
-}
-
-export default ContactList;
+      users.map(profile => (
+        <div className="Contact">
+          <img class='avatar' src={profile.avatar}/>
+          <div className='status'>
+          <h4 className='name'>{profile.name}</h4>
+          <div className={this.state.online ? 'status-online': 'status-offline'} key={profile.name}></div>
+              <p className='status-text' onClick={event => {
+                  const offline = !this.state.online;
+                  this.setState({online:offline})
+              }} key={profile.name}>{this.state.online ? 'Online': 'Offline'} </p>
+          </div>
+      </div>
+          
+          )))
+      }
+      }
+      export default ContactList;
